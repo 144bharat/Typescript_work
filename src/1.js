5687d9d2a3af4ddadd3c53a71be5f605647ed367
144bharat <144bharatgautam@gmail.com>
2023-02-26 19:55:50 +0530

    second commit

--- src/1.js
+++ src/1.js
@@ -10,3 +10,22 @@ var add = function (a, b) {
 add(10, 5);
 // add(10,"bharat");    error
 console.log(add(10, 5));
+// Declaring objects.
+// we have 3 types here 
+// Object
+// Object literal
+// Record
+var user = {
+    id: 1,
+    name: "bharat"
+};
+// user = {lastname:"kumar"}; //no error but bad practice.
+var user2 = {
+    id: 1,
+    name: "bharat"
+};
+var user3 = {
+    isIndian: true,
+    isName: true,
+    isEmployed: false
+};
--- src/1.ts
+++ src/1.ts
@@ -16,3 +16,29 @@ add(10,5);
 // add(10,"bharat");    error
 console.log(add(10,5));
 
+// Declaring objects.
+
+// we have 3 types here 
+        // Object
+        // Object literal
+        // Record
+let user:Object = {
+    id:1,
+    name:"bharat",
+}
+// user = {lastname:"kumar"}; //no error but bad practice.
+
+let user2:{id:number;name:string;} = {  // lastname:string;
+    id:1,
+    name:"bharat",
+                        // error lastname not present here.
+}
+
+let user3:Record<string,boolean> = {    // Record< key datatype , value datatype > only accepts.
+    isIndian:true,
+    isName:true,
+    isEmployed:false,
+    // isMoney:500,     // error only accepts boolean values.
+}
+
+
