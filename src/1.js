66350855228705c69bac23ba92a78da14c56b8c2
144bharat <144bharatgautam@gmail.com>
2023-02-26 19:08:15 +0530

    second commit

--- src/1.js
+++ src/1.js
@@ -1,2 +1,12 @@
 // Declaring variable.
 var a = 10;
+// a="bharat";  error
+// Declaring Array.
+var arr = [1, 2, 3];
+// Declaring functions.
+var add = function (a, b) {
+    return a + b;
+};
+add(10, 5);
+// add(10,"bharat");    error
+console.log(add(10, 5));
--- src/1.ts
+++ src/1.ts
@@ -1,2 +1,18 @@
 // Declaring variable.
 let a:number=10;
+// a="bharat";  error
+
+// Declaring Array.
+
+let arr:number[]=[1,2,3];
+
+// Declaring functions.
+
+const add = (a:number,b:number):number => {
+return a+b;
+}
+
+add(10,5);
+// add(10,"bharat");    error
+console.log(add(10,5));
+
